868598a67e286e4de9d4ecc4c09883f2f249c1e3
artemyyu <wrestlingteam57@gmail.com>
2025-01-29 10:22:58 +1100

    first commit

---
+++ game/src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(boardWidth, boardHeight);
+        frame. setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth,boardHeight);
+        frame.add(snakeGame);
+        snakeGame.requestFocus();
+
+    }
+}
